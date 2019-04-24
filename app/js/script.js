c7bfe811d613838ff454334b08533a0064e38577
c9t <c9t@protonmail.com>
2019-04-24 23:48:03 +0300

    Initial commit

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    var navigationBurger = $('.navigation_burger'),
+        headerMenu = $('.header_menu__content');
+
+    navigationBurger.on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('nav_add');
+
+        headerMenu.toggleClass('open');
+    })
+});
